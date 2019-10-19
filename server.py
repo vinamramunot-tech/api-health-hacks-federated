4513fd973b63880a1cbb49ec789fa2c286868b32
Carlos Alba <soradev4@gmail.com>
2019-10-19 15:31:49 -0700

    uploads route

--- server.py
+++ server.py
@@ -1,9 +1,49 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
+
 application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@application.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('no file part')
+            return redirect("/")
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect("/")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(
+                os.path.join(application.config['UPLOAD_FOLDER'], filename))
+            return redirect('/upload')
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
